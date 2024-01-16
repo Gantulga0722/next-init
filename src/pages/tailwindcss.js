9c79cbe79cf257c13e683764857b3ab60b19f111
Gantulga0722 <mgtulga22@gmail.com>
2024-01-16 12:07:21 +0800

    From Pinecone

---
+++ src/pages/tailwindcss.js
@@ -0,0 +1,8 @@
+export default function Tailwind() {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-700 border p-4 flex justify-center">
+      Hello world!
+    </h1>
+  );
+}
+
