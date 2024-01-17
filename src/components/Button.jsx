cca6c7017948891484efd16cb8420f92d8a7bada
Gantulga0722 <mgtulga22@gmail.com>
2024-01-17 21:41:20 +0800

    From Home

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "@/styles/components/buttons.module.css";
+
+export function Button() {
+  return <button className={styles.test}>Button</button>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,35 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Tailwind from "./tailwindcss";
-// import styles from "@/styles/Home.module.css";
+import { Button, TextField, Card } from "@/components";
+import Link from "next/link";
+import styles from "@/styles/pages/Home.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const input1 = document.getElementById("input");
+  const [todos, setTodos] = useState([]);
+  async function getTodos() {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos?_limit=" + input1.value
+    );
+    const data = await response.json();
+    console.log(response);
+    setTodos(data);
+  }
+
   return (
-    <div>
-      <Tailwind />
+    <div className="p-5">
+      <h1 className={"text-green-500 text-xl"}>Hello</h1>
+      {todos.map((todo) => {
+        return (
+          <div className="card">
+            <h1 className="text-2xl">Title: {todo.title}</h1>
+            <p>Completed: {todo.completed}</p>
+          </div>
+        );
+      })}
+      <input className="flex border rounded w-20" id="input"></input>
+      <button onClick={getTodos} className={"border-2 p-2 rounded"}>
+        Get todos
+      </button>
     </div>
   );
 }
